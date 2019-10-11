ccec075253a39283fa06d0ea1b38195fcfeb1a6a
jose teran <joseantonioterancevallos@gmail.com>
2019-10-11 14:50:56 +0200

    Add homejs import footer logo styled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Logo from "./icons/Logo";
+import Footer from "./components/Footer";
+import styled from "styled-components";
+
+const Main = styled.main`
+  padding: 10px;
+  background-color: #f8f5f5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  overflow: auto;
+  width: 100vw;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <span></span>
+      <Logo />
+
+      <Footer />
+    </Main>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FooterBar() {
+  return <main></main>;
+}
